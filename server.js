29209486fbcf417731ee35f24f0a46fe4824f573
lmajetic <lukamajeticc@gmail.com>
2025-07-03 21:40:10 +0200

    Fixing path

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 const bcrypt = require('bcryptjs');
 const bodyParser = require('body-parser');
-
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
